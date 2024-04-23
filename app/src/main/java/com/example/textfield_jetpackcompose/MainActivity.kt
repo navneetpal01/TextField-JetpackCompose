fb6cde64a701171b4897cf899dcbc4ffe82e9ec3
navneet <navneetpalgithub@gmail.com>
2024-04-23 10:19:49 +0530

    23

---
+++ app/src/main/java/com/example/textfield_jetpackcompose/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.textfield_jetpackcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+        }
+    }
+
+}
+
+
+
+
+
+
